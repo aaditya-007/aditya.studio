e8f5045fe4ac947d7349b27bd118577063b6cba4
Aditya Tibrewal <aditya7bgp@gmail.com>
2020-07-18 19:32:56 +0530

    Added favicon title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Aditya Tibrewal'
-export const siteTitle = 'My Blog'
+export const siteTitle = 'Aditya Tibrewal'
 
 export default function Layout({ children, home }) {
   return (
@@ -13,13 +13,11 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Software engineer with passion to build things. This is my digital garden."
         />
         <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://aditya.studio/images/profile.png`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
